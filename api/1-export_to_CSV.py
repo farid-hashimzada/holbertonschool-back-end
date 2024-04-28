d7f0740006c4de7f517564399059e4ee5dc236ae
farid-hashimzada <hashimzadafarid@gmail.com>
2024-04-28 17:09:59 +0400

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
+"""
+Module for tasks
+"""
+import csv
+import requests
+from sys import argv
 
-"""
-    `Export to CSV` module
-"""
+
+def main(id):
+    """
+    Retrieves user information and todos based on the given user ID.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, id)
+    todo_url = "{}/todos?userId={}".format(base_url, id)
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todo_url).json()
+
+    csv_writer = csv.writer(open(f"{id}.csv", "w"), quoting=csv.QUOTE_ALL)
+
+    for task in todos:
+        csv_writer.writerow(
+            [user["id"], user["username"], task["completed"], task["title"]]
+        )
 
 
 if __name__ == "__main__":
-
-    import csv
-    import requests
-    from sys import argv
-
-    if len(argv) < 2:
-        exit()
-
-    todos = requests.get(f"https://jsonplaceholder.typicode.\
-com/todos?userId={argv[1]}").json()
-    username = requests.get(f"https://jsonplaceholder.\
-typicode.com/users/{argv[1]}").json().get('username')
-
-    csv_writer = csv.writer(open(f'{argv[1]}.csv', 'w'), quoting=csv.QUOTE_ALL)
-
-    for todo in todos:
-        csv_writer.writerow([todo.get('userId'), username,
-                             todo.get('completed'), todo.get('title')])
+    if len(argv) == 2:
+        id = int(argv[1])
+        main(id)
